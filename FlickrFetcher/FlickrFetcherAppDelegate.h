373881d9b2161bd1ee131c9bcaa389fdbce0eb84
Jeff Lam <sha0lin@alumni.carnegiemellon.edu>
2011-11-27 01:24:43 -0500

    Initial Commit

---
+++ FlickrFetcher/FlickrFetcherAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FlickrFetcherAppDelegate.h
+//  FlickrFetcher
+//
+//  Created by Jeffrey Lam on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrFetcherAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

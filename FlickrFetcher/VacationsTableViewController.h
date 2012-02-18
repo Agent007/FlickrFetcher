70ca50fa02a5a50ca381524c1f374c70998ea377
Jeff Lam <sha0lin@alumni.carnegiemellon.edu>
2012-02-17 23:41:22 -0500

    renamed VacationViewController to VacationsTableViewController

---
+++ FlickrFetcher/VacationsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VacationViewController.h
+//  FlickrFetcher
+//
+//  Created by Jeffrey Lam on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface VacationsTableViewController : CoreDataTableViewController
+
+@end
